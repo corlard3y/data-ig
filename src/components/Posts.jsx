d308153f452f031b0d014e456728270555528272
Kolade <corlardey@gmail.com>
2021-06-16 17:39:44 +0100

    checking

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Posts({ data }) {
     return (
-        <div className=''>
+        <div className='w-full p-2 m-4 border rounded-md'>
             <Link to={`/post/${data.id}`}>
-            <div>
-                {data.id}
+            <div className='font-bold'>
+                {/* {data.id} */}
                 {data.title}
             </div>
                 {data.text}
